63f0510cc116b3664e2074ba180562c2003aa749
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-02-13 18:16:57 -0300

    Add: Algoritmo para calcular dificuldade de criar uma even word;

--- Even word/Program.cs
+++ Even word/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(SolutionTerceiro("acbcbba") == 1 ? "Correto" : "Errado");
+        Console.WriteLine(SolutionTerceiro("axxaxa") == 2 ? "Correto" : "Errado");
+        Console.WriteLine(SolutionTerceiro("aaaa") == 0 ? "Correto" : "Errado");
+    }
+
+    internal static int SolutionTerceiro(string S)
+    {
+        var list = S.OrderBy(x => x).ToArray();
+        int remotions = 0;
+        int countLetter = 1;
+        for (int i = 1; i < list.Length; i++)
+        {
+            if (list[i] == list[i - 1])
+                countLetter++;
+            else if (countLetter % 2 != 0)
+            {
+                remotions++;
+                countLetter = 1;
+            }
+        }
+        return countLetter % 2 != 0 ? ++remotions : remotions;
+    }
+}
