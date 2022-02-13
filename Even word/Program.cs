b6722b95c72fa774b62eaed792d9f9a64525faaf
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-02-13 18:22:54 -0300

    Add: Namespace

--- Even word/Program.cs
+++ Even word/Program.cs
@@ -1,27 +1,30 @@
-﻿public class Program
+﻿namespace EvenWord
 {
-    public static void Main()
+    public class Program
     {
-        Console.WriteLine(SolutionTerceiro("acbcbba") == 1 ? "Correto" : "Errado");
-        Console.WriteLine(SolutionTerceiro("axxaxa") == 2 ? "Correto" : "Errado");
-        Console.WriteLine(SolutionTerceiro("aaaa") == 0 ? "Correto" : "Errado");
-    }
-
-    internal static int SolutionTerceiro(string S)
-    {
-        var list = S.OrderBy(x => x).ToArray();
-        int remotions = 0;
-        int countLetter = 1;
-        for (int i = 1; i < list.Length; i++)
+        public static void Main()
         {
-            if (list[i] == list[i - 1])
-                countLetter++;
-            else if (countLetter % 2 != 0)
-            {
-                remotions++;
-                countLetter = 1;
-            }
+            Console.WriteLine(SolutionTerceiro("acbcbba") == 1 ? "Correto" : "Errado");
+            Console.WriteLine(SolutionTerceiro("axxaxa") == 2 ? "Correto" : "Errado");
+            Console.WriteLine(SolutionTerceiro("aaaa") == 0 ? "Correto" : "Errado");
         }
-        return countLetter % 2 != 0 ? ++remotions : remotions;
+
+        internal static int SolutionTerceiro(string S)
+        {
+            var list = S.OrderBy(x => x).ToArray();
+            int remotions = 0;
+            int countLetter = 1;
+            for (int i = 1; i < list.Length; i++)
+            {
+                if (list[i] == list[i - 1])
+                    countLetter++;
+                else if (countLetter % 2 != 0)
+                {
+                    remotions++;
+                    countLetter = 1;
+                }
+            }
+            return countLetter % 2 != 0 ? ++remotions : remotions;
+        }
     }
 }
